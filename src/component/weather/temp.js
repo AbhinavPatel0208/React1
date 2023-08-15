abcf4742534c0502358020059390f97cfa727684
ABHINAV <abhinavpatel0208@gmail.com>
2023-08-15 17:34:52 +0530

    Update temp.js

--- src/component/weather/temp.js
+++ src/component/weather/temp.js
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=8e0da3db8c0dc4c0998560fc35773fbc`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={API_KEY}`;
 
       let res = await fetch(url);
       let data = await res.json();
